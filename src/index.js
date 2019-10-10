dd25932d25310fdda4c0f05c3390b491590035d4
user <zliden01@protonmail.com>
2019-10-10 21:24:12 +0300

    123

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@
  * @returns number of weeks needed for finish education
  */
 module.exports = function getTimeForEducation(
-    focus = 'family', 
+    focus = 'family',
     knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+    config = { family: 4 }
+) {
+    if (knowsProgramming = true) {
+        if (focus == 'family') { return 800 / 4; } else if (focus == 'friends') { return 800 / 10; } else if (focus == 'normal_life') { return 800 / 20; } else if (focus == 'profession') { return Math.round(800 / 30) } else if (focus == 'carrier') { return 800 / 40 } else if (focus == 'top_peformance') { return Math.ceil(800 / 60) }
+    } else if (knowsProgramming = false) {
+        if (focus == 'family') { return 800 / 2; }
+    }
+};
